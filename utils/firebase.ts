c7308b300ae2e860dc65031a484ba099f9ebded2
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-01-07 23:34:02 +0530

    Updated

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -6,11 +6,12 @@ import {
 import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
+// ✅ Load from ENV
 const firebaseConfig = {
-  apiKey: "AIzaSyDMQdeUdvA6_cJRSR_pwTls0dcx2LgoVFw",
-  authDomain: "devstreaks24.firebaseapp.com",
-  projectId: "devstreaks24",
-  appId: "1:561565930488:web:bf31117aed969fc99c3c0d",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,4 +20,4 @@ export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-export const db = getFirestore(app); // ✅ Firestore
+export const db = getFirestore(app);
